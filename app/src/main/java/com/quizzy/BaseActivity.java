780e20aa1934d4ed63284f2386b9bc274c9b9f5b
CuB1z <cub1zdev@gmail.com>
2025-11-25 19:15:17 +0100

    Feat: Enhance BaseActivity toolbar with app name title and logo display

--- app/src/main/java/com/quizzy/BaseActivity.java
+++ app/src/main/java/com/quizzy/BaseActivity.java
@@ -3,6 +3,7 @@ package com.quizzy;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -21,8 +22,11 @@ public class BaseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setIcon(R.mipmap.ic_launcher_round);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setIcon(R.mipmap.ic_launcher_round);
+            actionBar.setTitle(getString(R.string.app_name));
         }
     }
 

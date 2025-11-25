443d1b52083d9f6808b5e3b392323adb6c4a56ff
CuB1z <cub1zdev@gmail.com>
2025-11-25 20:04:57 +0100

    Feat: Display high score in ResultActivity, update UI layout and button color resources

--- app/src/main/java/com/quizzy/ResultActivity.java
+++ app/src/main/java/com/quizzy/ResultActivity.java
@@ -19,15 +19,16 @@ public class ResultActivity extends BaseActivity {
 
         // Reference UI elements
         TextView scoreText = findViewById(R.id.finalScoreText);
+        TextView maxScoreText = findViewById(R.id.maxScoreText);
 
         SQLiteService sqLiteService = new SQLiteService(this);
 
         // Get final score from intent and display it
         int finalScore = getIntent().getIntExtra(Constants.FINAL_SCORE, 0);
         int maxScore = sqLiteService.getMaxScore();
-        System.out.println("Max Score from DB: " + maxScore);
 
         scoreText.setText(getString(R.string.result_score, finalScore));
+        maxScoreText.setText(getString(R.string.max_score, maxScore));
     }
 
     /**
